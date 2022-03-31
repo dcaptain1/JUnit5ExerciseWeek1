62277ae150944e17f5c875e2ce3572794d264b62
Gustav <gushol-0@student.ltu.se>
2022-03-31 11:24:36 +0200

    1

--- src/test/java/com/in28minutes/junit5/StringTest.java
+++ src/test/java/com/in28minutes/junit5/StringTest.java
@@ -3,18 +3,54 @@ package com.in28minutes.junit5;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringTest {
 
     @Test
-    void test(){
+    void length_basic(){
         //Write test code here :)
         //Invoke method
         //Make checks to see if its actually working
 
+        int actualLength = "ABCD".length();
+        int expectedLength = 4;
+
+        assertEquals(expectedLength,actualLength);
+
 
     }
+
+    @Test
+    void toUpperCase(){
+
+        String str = "abcd";
+        String result = str.toUpperCase();
+
+
+        assertNotNull(result);
+        //assertNull(result);
+        assertEquals("ABCD", result);
+
+
+    }
+
+    @Test
+    void contains_basic(){
+        String str = "abcdefgh";
+        Boolean result = str.contains("ijk");
+
+        assertEquals(false,result);
+        assertFalse(result);
+
+    }
+
+
+
+
+
 
 
 
