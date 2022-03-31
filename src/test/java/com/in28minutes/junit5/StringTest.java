9adaa360df6f0477dd794d46dc5d8927883d57e7
Gustav <gushol-0@student.ltu.se>
2022-03-31 10:52:34 +0200

    1

---
+++ src/test/java/com/in28minutes/junit5/StringTest.java
@@ -0,0 +1,21 @@
+package com.in28minutes.junit5;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    void test(){
+        //Write test code here :)
+        //Invoke method
+        //Make checks to see if its actually working
+
+
+    }
+
+
+
+}
